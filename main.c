0202505b7e6e83f9c3b53283a3ea1da13fa2b44f
Wilsen Hernandez <wilsenh95@gmail.com>
2018-03-16 19:03:20 -0400

    Commit inicial, definicion programa inicial

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * Universidad de Carabobo
+ * Facultad de Ciencias y Tecnología
+ * Sistemas Operativos
+ * Proyecto Semestre I-2017
+ *
+ * Freddy Duran
+ * Michelle Sanseviero
+ * Wilsen Hernández
+ * */
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
