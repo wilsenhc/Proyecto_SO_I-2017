e6cfbb7bd92dd7ae578b68b7061030b6d0cfa900
Maurizio <maurizio_1195@hotmail.com>
2018-03-16 22:40:43 -0400

    listo

--- main.c
+++ main.c
@@ -21,10 +21,7 @@ typedef struct {
 } Jacobi;
 
 
-/*void matriz_sup(double **matrz, double **matrz_sup, int N);
-void matriz_inf(double **matrz,double **matrz_inf,int N);
-void matriz_diag(double **matrz,double **matrz_diag,int N);
-void inicializar_matr(double **matriz,int N);*/
+
 void jcobi(Jacobi J);
 double sumatoria(Jacobi J,int i);
 int comprobar(Jacobi J);
@@ -74,70 +71,6 @@ int main(int argc, char** argv)
     return 0;
 }
 
-//matrz = matriz original, matrz_sup = matriz con solo la triangular superior de la matriz original
-// N = tamaño de la matriz
-/*(matr_sup debe de estar inicializada en 0 antes de llamar a este procedimiendo)*/
-/*void matriz_sup(double **matrz, int **matrz_sup, int N)
-{
-	int i,j;
-
-	for(i = 0;i < N ; i++)
-	{
-		for(j = i+1;j < N ; j++)
-		{
-			matrz_sup[i][j] = matrz[i][j];
-
-		}
-	}
-}
-*/
-//matrz = matriz original, matrz_inf = matriz con solo la triangular inferior de la matriz original
-// N = tamaño de la matriz
-/*(matr_inf debe de estar inicializada en 0 antes de llamar a este procedimiendo)*/
-
-/*void matriz_inf(double **matrz,int **matrz_inf,int N)
-{
-	int i,j;
-
-	for(i = 0;i < N ; i++)
-	{
-		for(j = i+1;j < N ; j++)
-		{
-			matrz_inf[j][i] = matrz[j][i];
-		}
-	}
-}
-*/
-//matrz = matriz original, matrz_diag = matriz con solo la diagonal de la matriz original
-// N = tamaño de la matriz
-/*(matr_diag debe de estar inicializada en 0 antes de llamar a este procedimiendo)*/
-/*
-void matriz_diag(double **matrz,double **matrz_diag,int N)
-{
-	int i,j;
-
-	for(i = 0;j < N ; i++)
-	{
-		matrz_diag[i][i] = matrz[i][i];
-	}
-}
-*/
-/*matriz = matriz a inicializar, N = tamaño de la matriz*/
-/*
-void inicializar_matr(double **matriz,int N)
-{
-	int i,j;
-
-	for(i = 0;i < N;i++)
-	{
-		for(j = 0;j < N;j++)
-		{
-			matriz[i][j] = 0;
-		}
-	}
-
-}
-*/
 
 void jcobi(Jacobi J)
 {
