b85b1587ea8d2b616b2376f9299a78c7732712fe
Wilsen Hernandez <wilsenh95@gmail.com>
2018-03-16 20:16:10 -0400

    Lectura de datos por entrada estandar

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/*
+/**
  * Universidad de Carabobo
  * Facultad de Ciencias y Tecnología
  * Sistemas Operativos
@@ -10,8 +10,54 @@
  * */
 #include <stdio.h>
 
+struct Jacobi {
+    int dimensionMatriz;
+    int maximoIteraciones;
+    double tolerancia;
+    double **matrizA;
+    double *vectorB, *vectorX, *vectorXInicial;
+};
+
 int main(int argc, char** argv)
 {
+    Jacobi jacobi;
+
+    /**
+     * Lectura de archivo de entrada
+     * */
+    scanf(" %d", &jacobi.dimensionMatriz);
+
+    jacobi.matrizA = (double*) calloc(jacobi.dimensionMatriz, sizeof(double*));
+
+    for (int i = 0; i < jacobi.dimensionMatriz; i++)
+    {
+        jacobi.matrizA[i] = (double) calloc(jacobi.dimensionMatriz, sizeof(double));
+    }
+
+    jacobi.vectorB = (double) calloc(jacobi.dimensionMatriz, sizeof(double));
+    jacobi.vectorX = (double) calloc(jacobi.dimensionMatriz, sizeof(double));
+    jacobi.vectorXInicial = (double) calloc(jacobi.dimensionMatriz, sizeof(double));
+
+    scanf(" %f", &jacobi.tolerancia);
+    scanf(" %d", &jacobi.maximoIteraciones);
+
+    for (int i = 0; i < jacobi.dimensionMatriz; i++)
+    {
+        scanf(" %f", &jacobi.vectorXInicial[i]);
+    }
+
+    for (int i = 0; i < jacobi.dimensionMatriz; i++)
+    {
+        for (int j = 0; j < jacobi.dimensionMatriz; j++)
+        {
+            scanf(" %f", &jacobi.matrizA[i][j]);
+        }
+    }
+
+    for (int i = 0; i < jacobi.dimensionMatriz; i++)
+    {
+        scanf(" %f", &jacobi.vectorB[i]);
+    }
 
     return 0;
 }
